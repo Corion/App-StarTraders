f881992bd117916f301c0fa7d4619a95990062dc
Max Maischein <corion@corion.net>
2009-03-13 19:40:11 +0100

    First cut at IsPlace role

--- lib/App/StarTraders/Role/IsPlace.pm
+++ lib/App/StarTraders/Role/IsPlace.pm
@@ -2,10 +2,40 @@ package App::StarTraders::Role::IsPlace;
 use strict;
 use Moose::Role;
 
-has name => (
-    is => 'rw',
-    isa => 'Str',
-    default => 'unnamed entity',
+with 'App::StarTraders::Role::HasName';
+has '+name' => ( default => 'unnamed place' );
+
+has children => (
+    is => 'ro',
+    isa => 'ArrayRef',
+    default => sub { [] },
+    auto_deref => 1,
 );
+
+has parent => (
+    is => 'ro',
+    does => 'IsPlace',
+    auto_deref => 1,
+);
+
+=head2 C<< ->enter >>
+
+Broadcasts the message that an object enters this place
+(or one of its subplaces)
+
+=cut
+
+#sub enter {
+#    my ($self,$obj) = @_;
+#};
+
+=head2 C<< ->leave >>
+
+Broadcasts the message that an object leaves this place
+(or one of its subplaces)
+
+=cut
+#sub leave {};
+
 
 1;
