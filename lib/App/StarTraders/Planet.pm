94bf46a28930c0e6e8655156e5df6ecc1765c613
Max Maischein <corion@corion.net>
2009-03-22 16:39:32 +0100

    More code cleanup, as the IsPlace role consumes more of ::Planet

--- lib/App/StarTraders/Planet.pm
+++ lib/App/StarTraders/Planet.pm
@@ -13,20 +13,4 @@ __PACKAGE__->meta->make_immutable;
 
 *system = \&parent;
 
-# How can we orbit this planet?
-# This planet (or any place) should know about ships in/on it
-
-sub can_release { 1 };
-sub can_receive { 1 };
-
-#sub receive {
-#    my ($self,$ship) = @_;
-#    print sprintf "Ship %s now orbits %s\n", $ship->name, $self->name;
-#};
-
-#sub release {
-#    my ($self,$ship) = @_;
-#    print sprintf "Ship %s leaves %s orbit\n", $ship->name, $self->name;
-#};
-
 1;
