a6771f75fe2cae7827680d276d7dd7cd78771f96
Max Maischein <corion@corion.net>
2009-03-26 23:18:50 +0100

    Added capacity()

--- lib/App/StarTraders/Role/IsContainer.pm
+++ lib/App/StarTraders/Role/IsContainer.pm
@@ -2,6 +2,12 @@ package App::StarTraders::Role::IsContainer;
 use strict;
 use Moose::Role;
 
+has capacity => (
+    is => 'rw',
+    isa => 'Integer',
+    default => 0,
+);
+
 # This will belong to IsItemStack (or ItemStack?)
 # A container contains multiple ItemStacks
 
@@ -16,5 +22,18 @@ has item => (
     isa => 'String',
     default => 'Nothing',
 );
+
+=head2 C<< ->capacity_used >>
+
+Returns the used capacity of this container.
+
+As long as we use integral units of item-capacity,
+this will be an easy calculation.
+
+=cut
+
+sub capacity_used { $_[0]->quantity };
+
+sub capacity_free { $_[0]->capacity - $_[0]->capacity_used };
 
 1;
