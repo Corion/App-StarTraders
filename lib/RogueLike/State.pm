eae8203566b00f6f7f91ed0fed33cea392b2553c
Max Maischein <corion@corion.net>
2014-08-08 18:35:54 +0200

    Move actor sequence code into its own routine
    Also, add a stub listing the actors ready to act

--- lib/RogueLike/State.pm
+++ lib/RogueLike/State.pm
@@ -19,6 +19,31 @@ sub can_enter( $self, $actor, $position ) {
 
 sub add_actor( $self, @actors ) {
     push @{ $self->actors }, @actors;
+    
+    $self->rebuild_actors;
+}
+
+# Assumes that the first actor has just spent energy (or not)
+# This should become a priority queue for speed
+sub rebuild_actors( $self, $actor ) {
+    my $actors= $self->actors;
+    
+    warn "Rebuilding actor order";
+
+    # Put the actor that just acted at the end of its (new) priority queue
+    if( $actor ) {
+        @$actors= grep { $_ != $actor } @$actors;
+        push @$actors, $actor;
+    };
+    
+    # Well, a real priority queue insert would be better, but...
+    @$actors= sort { $b->energy <=> $a->energy } (@{ $actors });
+    
+    $actors
+}
+
+# Return all actors with enough enery, in the "preferred" order
+sub ready_to_act( $self ) {
 }
 
 # A BSP / quadtree, or just brute force?
