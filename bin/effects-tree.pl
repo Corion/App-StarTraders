ce1217a263cd21d7bbf89dbf30a84a8512524be0
Maximilian Maischein <corion@corion.net>
2015-09-05 19:29:11 +0200

    Make the actor its own class, move some subroutines to methods

--- bin/effects-tree.pl
+++ bin/effects-tree.pl
@@ -67,6 +67,86 @@ sub max($self) {
     $self->{_max}
 }
 
+package RPG::StatsActor;
+use strict;
+use Moo::Lax;
+
+# The effects currently active on the player (or wherever)
+has 'active_effects' => (
+    is => 'ro',
+    default => sub { [] },
+);
+
+sub add_effect($self,@effects) {
+    push @{$self->active_effects},@effects;
+}
+
+# Formula is
+# base*(1+sum(scale)) + sum(buffs)
+# Only UNMODIFIED attributes are taken into consideration, to prevent
+# stacking loops where attribute a affects attribute b, which next turn
+# affects attribute a again (except usually more complicated)
+sub get_effects( $self, $base, $effects ) {
+    for my $effect ( @{ $self->active_effects }) {
+        $effect->apply( $base, $effects );
+    }
+};
+
+sub get_effects_delta ( $self, $base ) {
+    $self->get_effects( $base, \my %effects );
+
+    \%effects
+};
+
+# This is where life choices accumulate, like (randomized) gains through
+# levelling or permanent bonuses from consuming items
+# Why not have them as a list too?! Prioritize that list/sort it after expiry
+# and caching becomes possible. The Journal is everywhere.
+# Consider "baking"/consolidating the effects. At least all permanent effects
+# can be consolidated, as can all effects lasting longer than some certain
+# tick period or until the inventory gets changed.
+# Maybe the caching should be considered only after it really becomes a problem.
+# If this scheme gets applied to items too (like recharging fireball swords),
+# then caching will become an issue.
+# Note that stats are only interesting upon player actions (combat,skill checks)
+# so maybe having the whole thing be recalculated dynamically isn't actually
+# that big a deal
+
+sub apply_effects( $self, $effects_delta ) {
+    my $base = $self->get_base_attributes;
+
+    # This should basically be a delegating objects to $player, I think
+    my %res;
+    # $player should have a method listing all attributes instead of
+    # us iterating over the keys...
+    # And really, we should think of caching early, that is, caching a
+    # "generation" of the player so we don't recalculate. But hunger is
+    # ever-present, so we'll always recalculate?!
+    for my $attr (keys %$effects_delta) {
+        if( $effects_delta->{ $attr }) {
+            $res{ $attr } = ($base->{$attr}|| 0) * ($effects_delta->{$attr}->{scale} || 1)+ ($effects_delta->{$attr}->{buff} || 0); 
+        } else {
+            $res{ $attr } = $base->{$attr}; 
+        }
+    }
+
+    # Cap negative numbers to zero
+    # Maybe we should have this per-attribute
+    for( values %res) {
+        $_ = 0 if $_ < 0
+    }
+
+    \%res
+}
+
+sub remove_effects( $self, $crit ) {
+    @{ $self->active_effects } = grep { !$crit->() } @{ $self->active_effects };
+};
+
+sub get_base_attributes( $self ) {
+    {}
+}
+
 package main;
 use strict;
 use Carp qw(croak);
@@ -99,8 +179,8 @@ sub roll($dice) {
     $sum
 }
 
-# The effects currently active on the player (or wherever)
-@active_effects = (map {RPG::Effect->new(%$_)} (
+my $player = RPG::StatsActor->new(
+    active_effects => [map {RPG::Effect->new(%$_)} (
     # Initial "effects" basically calculating the base player stats
     # Having them here means additional work when displaying the stats?!
         { name => 'roll.intelligence', base_attribute => '', affected_attribute => 'intelligence', buff => roll('3d6') },
@@ -119,83 +199,37 @@ sub roll($dice) {
         { name => 'vigorous_regeneration', base_attribute => 'constitution', affected_attribute => 'health', buff => 0, scale => 0.10 },
         { name => 'regeneration', base_attribute => 'intelligence', affected_attribute => 'mana', buff => 1, scale => 0 },
         { name => 'ring of mana regeneration', base_attribute => '', affected_attribute => 'mana', buff => 1, scale => 0, ratio => 'per tick' },
-    )
-);
+    )]);
 use Data::Dumper;
 #warn Dumper \@active_effects;
 
-# Formula is
-# base*(1+sum(scale)) + sum(buffs)
-# Only UNMODIFIED attributes are taken into consideration, to prevent
-# stacking loops where attribute a affects attribute b, which next turn
-# affects attribute a again (except usually more complicated)
-sub get_effects( $base, $effects, @active_effects ) {
-    for my $effect ( @active_effects ) {
-        $effect->apply( $base, $effects );
-    }
-};
-
-sub get_effects_delta ( $tick_count, $base, @active_effects ) {
-    get_effects( $base, \my %effects, @active_effects );
-
-    \%effects
-};
-
-# These are the base stats of the player
-# unadorned by effects
-# player_body -> life_choices/adventure buffs/permanent effects, like levelling -> effects
-my %player = (
-    effects => \@active_effects, # this calculates the maximum
-    current => undef,
-);
-
-# This is where life choices accumulate, like (randomized) gains through
-# levelling or permanent bonuses from consuming items
-# Why not have them as a list too?! Prioritize that list/sort it after expiry
-# and caching becomes possible. The Journal is everywhere.
-# Consider "baking"/consolidating the effects. At least all permanent effects
-# can be consolidated, as can all effects lasting longer than some certain
-# tick period or until the inventory gets changed.
-# Maybe the caching should be considered only after it really becomes a problem.
-# If this scheme gets applied to items too (like recharging fireball swords),
-# then caching will become an issue.
-# Note that stats are only interesting upon player actions (combat,skill checks)
-# so maybe having the whole thing be recalculated dynamically isn't actually
-# that big a deal
-
-my $active_effects= get_effects_delta(undef, {}, @active_effects);
-
-sub apply_effects( $player, $effects_delta ) {
-    # This should basically be a delegating objects to $player, I think
-    my %res;
-    # $player should have a method listing all attributes instead of
-    # us iterating over the keys...
-    # And really, we should think of caching early, that is, caching a
-    # "generation" of the player so we don't recalculate. But hunger is
-    # ever-present, so we'll always recalculate?!
-    for my $attr (keys %$effects_delta) {
-        if( $effects_delta->{ $attr }) {
-            $res{ $attr } = ($player->{$attr}|| 0) * ($effects_delta->{$attr}->{scale} || 1)+ ($effects_delta->{$attr}->{buff} || 0); 
-        } else {
-            $res{ $attr } = $player->{$attr}; 
-        }
-    }
-    \%res
-}
-
-$player{ current } = apply_effects({}, $active_effects);
+my $active_effects= $player->get_effects_delta(undef, {});
+$player->{ current } = $player->apply_effects($active_effects);
 # we should strip out all attributes where max=current according to the rules
-warn Dumper \$player{current};
+warn Dumper \$player->{current};
 
-push @active_effects, RPG::Effect->new(
-        { name => 'dunce cap', base_attribute => 'intelligence', affected_attribute => 'intelligence', buff => -15, scale => 0 },
-);
+# Wear a dunce cap
+$player->add_effect( RPG::Effect->new(
+    { name => 'dunce cap', base_attribute => 'intelligence', affected_attribute => 'intelligence', buff => -15, scale => 0 },
+));
 
-$active_effects= get_effects_delta(undef, {}, @active_effects);
-warn Dumper $active_effects;
-$player{ current } = apply_effects({}, $active_effects);
+$active_effects= $player->get_effects_delta({});
+#warn Dumper $active_effects;
+$player->{ current } = $player->apply_effects($active_effects);
 # we should strip out all attributes where max=current according to the rules
-warn Dumper \$player{current};
+warn Dumper \$player->{current};
+
+# Remove the dunce cap now
+$player->remove_effects(sub{ $_->{name} eq 'dunce cap'});
+
+$active_effects= $player->get_effects_delta({});
+#warn Dumper $active_effects;
+$player->{ current } = $player->apply_effects($active_effects);
+# we should strip out all attributes where max=current according to the rules
+warn Dumper \$player->{current};
+
+
+# How will effects be attached/connected with the items?!
 
 # How to structure the "lensed" attributes?
 # We have lenses for "base", "current" and "base maximum" and "current maximum",
