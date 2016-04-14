5895bd685924daac437e1f732765178adcc18aea
Max Maischein <corion@corion.net>
2016-04-14 20:35:20 +0200

    More implementation of HTTP UI

--- lib/RogueLike/HTTP.pm
+++ lib/RogueLike/HTTP.pm
@@ -78,19 +78,31 @@ sub get_roster($g) {
 };
 
 get '/waiting' => sub {
-    return [
-        grep { $_->{waiting} } @{get_roster($g)}
-    ]
+    if( $g ) {
+        return [
+            grep { $_->{waiting} } @{get_roster($g)}
+        ]
+    } else {
+        # No game running, nothing to do
+        return []
+    };
 };
 
 get '/choose' => sub {
-    template 'choose', { roster => get_roster($g) };
+    if( $g ) {
+        template 'choose', { roster => get_roster($g) };
+    } else {
+        # No game running, no list to show
+        template 'choose', { roster => [] };
+    }
 };
 
+my %connections;
 post '/choose' => sub {
     my $player = params->{player};
     session player => $player;
     print STDERR "Activating player '$player'\n";
+    $connections{ session->{sid} } = $player;
     return redirect '/';
 };
 
@@ -124,6 +136,76 @@ get '/' => sub {
         roster => get_roster($g),
         player => $for,
     };
+};
+
+# This could become a nice? "interactive" framework:
+#
+# event: refresh fragment: #foo
+# -or-
+# event: refresh fragment: #foo from: /fragments/foo
+# 
+# for global fragments like sidebar
+
+# On the server side, we need a triple association between
+# #foo, /fragments/foo and generate_foo(), which is inconvenient.
+# /fragments/fragment_map.json could provide that map to the client
+# automatically so that changes in the site template don't necessitate
+# changes in the JS client.
+
+get '/events' => sub {
+    # This expects a simple JavaScript eval-loop at the other end
+
+    # Capture some Dancer information for later
+    my $sid= session->{id};
+    my $username= user->{displayname};
+    my $player = get_player( session->{player});
+
+    my $backlog= request->env->{"HTTP_LAST_EVENT_ID"};
+    # We don't actually use the backlog. We simply make the client
+    # request the complete current state.
+
+    # immediately start the response and stream the content
+    status 200;
+    header("Content-Type", "text/event-stream");
+    #header("Content-Length", "1000000"); # XXX Patch Dancer to not add a content length to streamed responses!
+    event_stream(sub {
+        my $writer= shift;
+        # This is what we use to talk to the client
+        my $listener; $listener= sub {
+            my $event= HTTP::ServerEvent->as_string(
+                data => encode_json($_[1]),
+                event => 'update',
+                id => $_[0],
+                retry => $retry_ms,
+            );
+
+            if(! eval {
+                $writer->write($event);
+                1;
+            }) { # Session has disconnected
+                warn "Error while writing event: $@";
+                delete $connections{ $sid };
+                undef $listener;
+                undef $writer;
+                
+                # XXX send all other clients an "this client is unavailable"
+                #     message
+
+            };
+        };
+
+        $connections{ $user->{uid} }->{ $sid }= $listener;
+        # XXX Maybe we should clean out eventual old sessions+connections here
+
+        # XXX Should we also support partial configs? Like only sending the nickname, or avatar image?
+        #unshift @setup, {action => 'configure', config => $session_info };
+
+        # Send the current roster for the (one) channel
+        push @setup, get_roster_actions( @channels );
+
+        my $ts= strftime "%Y-%m-%d %H:%M:%S", gmtime;
+
+    });
 };
 
 1;
